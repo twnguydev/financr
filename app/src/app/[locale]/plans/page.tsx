588156a2fb1bad066b69063584060181efaaf331
Tanguy Gibrat <gibratanguy@icloud.com>
2024-10-03 22:47:56 +0200

    delete imports i18next

--- app/src/app/[locale]/plans/page.tsx
+++ app/src/app/[locale]/plans/page.tsx
@@ -1,6 +1,4 @@
 // src/app/page.tsx
-import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
 import Subscriptions from '@/components/landing-page/Subscriptions';
 
 export default function Home(): JSX.Element {
--- app/src/app/[locale]/use-cases/page.tsx
+++ app/src/app/[locale]/use-cases/page.tsx
@@ -1,6 +1,4 @@
 // src/app/page.tsx
-import { appWithTranslation } from 'next-i18next';
-import type { AppProps } from 'next/app';
 import UseCases from '@/components/landing-page/UseCases';
 
 export default function Home(): JSX.Element {
