d974bb7c552652b753e5c6105937204477d85d18
Tanguy Gibrat <gibratanguy@icloud.com>
2024-10-03 22:52:27 +0200

    fix: i18n static rendering

--- app/src/i18n/request.ts
+++ app/src/i18n/request.ts
@@ -1,9 +1,11 @@
 import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
+import { getRequestConfig, unstable_setRequestLocale } from 'next-intl/server';
 import { routing } from '@/i18n/routing';
 
 export default getRequestConfig(async ({ locale }) => {
   if (!routing.locales.includes(locale as any)) notFound();
+
+  unstable_setRequestLocale(locale);
 
   return {
     messages: (await import(`/messages/${locale}.json`)).default
