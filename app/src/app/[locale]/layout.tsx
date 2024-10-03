37f0c6847cc6316b1af152ba6f0868eeac92ced1
Tanguy Gibrat <gibratanguy@icloud.com>
2024-10-03 22:57:21 +0200

    fix: i18n client side rendering

--- app/src/app/[locale]/layout.tsx
+++ app/src/app/[locale]/layout.tsx
@@ -1,14 +1,15 @@
 // /app/[lang]/layout.tsx
 import { NextIntlClientProvider } from 'next-intl';
-import { getTranslations } from 'next-intl/server';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 import { ReactNode } from "react";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { routing } from '@i18n/routing';
 
 export function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'fr' }];
+  return routing.locales.map((locale) => ({locale}));
 }
  
 export async function generateMetadata({params: { locale }}: { params: { locale: string } }) {
@@ -34,6 +35,8 @@ export default async function RootLayout({
   children: ReactNode;
   params: { locale: string };
 }) {
+  unstable_setRequestLocale(locale);
+
   let messages;
   try {
     messages = (await import(`/messages/${locale}.json`)).default;
